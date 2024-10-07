88e585fe6ef3b281a4b8c2b11ca9eea29069de5a
Shaikh Mohammad Umar Farooq <shaikhumar06122001@gmail.com>
2024-10-07 22:30:48 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("Welecome to Book Bug - an E-book api");
+import app from "./src/app";
+
+const startServer = () => {
+    const port = process.env.PORT || 5000;
+
+    app.listen(port, () => {
+        console.log(`listening on port: ${port}`);
+    });
+};
+
+startServer();
