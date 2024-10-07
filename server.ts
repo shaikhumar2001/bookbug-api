36124e2ed51d4cc7e0b49b9ac25cfc9d7f08c0ad
Shaikh Mohammad Umar Farooq <shaikhumar06122001@gmail.com>
2024-10-07 23:11:33 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import app from "./src/app";
+import { config } from "./src/config/config";
 
 const startServer = () => {
-    const port = process.env.PORT || 5000;
+    const port = config.port || 5000;
 
     app.listen(port, () => {
         console.log(`listening on port: ${port}`);
