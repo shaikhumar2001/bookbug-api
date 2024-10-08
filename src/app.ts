8ce3c64b542dc952707692cf3137546e06e44444
Shaikh Mohammad Umar Farooq <shaikhumar06122001@gmail.com>
2024-10-08 16:02:05 +0000

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from "express";
+import createHttpError from "http-errors";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
 const app = express();
 
+app.use(express.json());
+
 // Routes
 app.get("/", (req, res, next) => {
+    throw createHttpError(400, "something went wrong!");
     res.json({ message: "Welcome to Book Bug api" });
 });
+
+// Global Error Handler
+app.use(globalErrorHandler);
 
 export default app;
